51821d25e171afc82d258db7312a1582337e8553
edgar <edgartc03@gmail.com>
2025-09-16 21:29:31 +0530

    updated ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -21,25 +21,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
   constructor(private router: Router) {}
 
-  // Check if user is logged in
   get isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
   }
 
-  // Get username from localStorage (set during login)
+  get isAdmin(): boolean {
+    return localStorage.getItem('role') === 'Admin';
+  }
+
   get userName(): string | null {
     return localStorage.getItem('username');
   }
 
-  goToMenu() {
-    this.router.navigate(['/menu']);
-  }
-
   logout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('username'); // clear username on logout
-    this.router.navigate(['/']); // Navigate to home after logout
+    localStorage.clear(); // remove token, username, role
+    this.router.navigate(['/']);
   }
 }
